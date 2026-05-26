e7a0f43b3d558d24c185e9ef33f535b53fce9865
Nguyen The Hieu <nguyenhieu03092005@gmail.com>
2026-05-26 13:39:24 +0700

    update frontend

--- SE2033_JV_Team6_ProjectWeb/backend/src/main/java/com/mycompany/jpademo/backend/controller/MedicalRecordController.java
+++ SE2033_JV_Team6_ProjectWeb/backend/src/main/java/com/mycompany/jpademo/backend/controller/MedicalRecordController.java
@@ -2,20 +2,25 @@ package com.mycompany.jpademo.backend.controller;
 
 import com.mycompany.jpademo.backend.dto.response.MedicalRecordDetailResponse;
 import com.mycompany.jpademo.backend.dto.response.MedicalRecordResponse;
+import com.mycompany.jpademo.backend.repository.DiagnosisSessionRepository;
 import com.mycompany.jpademo.backend.service.interfaces.MedicalRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/medical-records")
-@CrossOrigin(origins = "*")
+
 public class MedicalRecordController {
 
     @Autowired
     private MedicalRecordService medicalRecordService;
+    @Autowired
+    private DiagnosisSessionRepository sessionRepository;
 
     /**
      * API 1: Lấy danh sách lịch sử tất cả các ca khám cũ của một bệnh nhân
@@ -29,6 +34,15 @@ public class MedicalRecordController {
 
         return ResponseEntity.ok(detail);
     }
+    @GetMapping
+    public List<Map<String, Object>> getAllRecords() {
 
+        return sessionRepository.findAllMedicalRecords();
+    }
+    @GetMapping("/patient/{patientId}")
+    public List<Map<String, Object>> getRecordsByPatient(@PathVariable Integer patientId) {
+        // Trả về danh sách bệnh án riêng của bệnh nhân đó
+        return sessionRepository.findMedicalRecordsByPatientId(patientId);
+    }
 
 }
